f5c2a4ccba653c2bea71d0ae64a487217b14326a
JackArrowden <87233227+JackArrowden@users.noreply.github.com>
2023-10-04 22:15:58 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
+
 string SignedIntegerToSignMagnitude(int num) {
     long long tmp = 1 << 7;
-    if(num < -tmp + 1 || num > tmp - 1) return "overflow";
-    if(num == 0)
-    return "10000000 00000000";
+    if (num < -tmp + 1 || num > tmp - 1) return "overflow";
+    if (num == 0) return "10000000 00000000";
+
     int sign = (num < 0) ? 1 : 0;
     int magnitude = (num < 0) ? -num : num;
 
@@ -20,34 +22,54 @@ string SignedIntegerToSignMagnitude(int num) {
 
     return SignMagnitude;
 }
+
 string SignedIntegerToOneComplement(int num) {
     long long tmp = 1 << 7;
-    if(num < -tmp + 1 || num > tmp - 1) return "overflow";
-    if(num == 0)
-    return "11111111 00000000";
+    if (num < -tmp + 1 || num > tmp - 1) return "overflow";
+    if (num == 0) return "11111111 00000000";
+
     int sign = (num < 0) ? 1 : 0;
     int magnitude = (num < 0) ? -num : num;
 
     // Convert the magnitude to binary and pad to 7 bits
     string magnitudeBits;
     for (int i = 6; i >= 0; --i) {
-        if(sign == 0)
-        magnitudeBits += (magnitude & (1 << i)) ? '1' : '0';
-        else magnitudeBits+= (magnitude & (1 << i)) ? '0' : '1';
+        if (sign == 0) magnitudeBits += (magnitude & (1 << i)) ? '1' : '0';
+        else magnitudeBits += (magnitude & (1 << i)) ? '0' : '1';
     }
 
-   
-    
     // Combine the sign bit and inverted magnitude bits
     string oneComplement = to_string(sign) + magnitudeBits;
 
     return oneComplement;
 }
+
+string SignedIntegerToTwoComplement(int num) {
+    long long tmp = 1 << 7;
+    if (num < -tmp || num > tmp - 1) return "overflow";
+
+    int sign = (num < 0) ? 1 : 0;
+    int magnitude = (num < 0) ? -num - 1 : num;
+
+    // Convert the magnitude to binary and pad to 7 bits
+    string magnitudeBits;
+    for (int i = 6; i >= 0; --i) {
+        if (sign == 0) magnitudeBits += (magnitude & (1 << i)) ? '1' : '0';
+        else magnitudeBits += (magnitude & (1 << i)) ? '0' : '1';
+    }
+    
+    // Combine the sign bit and inverted magnitude bits
+    string twoComplement = to_string(sign) + magnitudeBits;
+
+    return twoComplement;
+}
+
 int main() {
     int num;
-    cin>>num;
-    cout<<"Output a: "<<SignedIntegerToSignMagnitude(num)<<endl;
-    cout<<"Output b: "<<SignedIntegerToOneComplement(num);
+    cout << "Input: "; cin >> num;
+    cout << "Output a: " << SignedIntegerToSignMagnitude(num) << endl;
+    cout << "Output b: " << SignedIntegerToOneComplement(num) << endl;
+    cout << "Output c: " << SignedIntegerToTwoComplement(num) << endl;
 
     return 0;
 }
